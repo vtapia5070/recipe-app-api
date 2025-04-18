9ac84b3b27d5bd06bd39fe8daec59b17147e32b2
victoria tapia <tapia.victoria5070@gmail.com>
2025-04-18 12:13:41 -0700

    code cleanup, fix tests

--- app/core/models.py
+++ app/core/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import (
   AbstractBaseUser,
   BaseUserManager,
-  PermissionsMixin
+  PermissionsMixin,
 )
 
 class UserManager(BaseUserManager):
@@ -17,11 +17,15 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
 
+        return user
+
 class User(AbstractBaseUser, PermissionsMixin):
     """Users in the system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,12 +2,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-class ModelTest(TestCase):
+class ModelTests(TestCase):
     """Test models."""
 
     def test_create_user_with_email_successful(self):
         """Test creating user with an email is successful."""
-        email = "test@example.com"
+        email = 'test@example.com'
         password = "testpass123"
         user = get_user_model().objects.create_user(
             email,
