4d9cd0f574d4d4e04a3969d21d04767f15addc99
victoria tapia <tapia.victoria5070@gmail.com>
2025-04-22 15:40:14 -0700

    set up user api and tests

---
+++ app/user/views.py
@@ -0,0 +1,10 @@
+"""
+Views for the user API.
+"""
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
